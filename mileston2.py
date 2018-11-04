9583b16cfc4c12ed178dd53aca3f60ab71c34e53
Aaron Gramlich <gramlich.aaron@gmail.com>
2018-11-03 17:32:32 -0700

    overwriting previous project with newer version

--- mileston2.py
+++ mileston2.py
@@ -1,230 +1,154 @@
 import random
-import math
 
-class Deck():
+class DealHand():
 
+#   max_total = 21
+#       moved max_total to CheckBust class
+    forced_hit = 16
+    turn = ''
+    full_deck = [2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,'10','10','10','10','J','J','J','J','Q','Q','Q','Q','K','K','K','K','A','A','A','A']
+    p_card = []
+    d_card = []
+    p_total_count = 0
+    d_total_count = 0
 
     def __init__(self):
-        '''
-        Builds a deck
-        '''
-        self.cards_in_deck = [2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,'J','J','J','J','Q','Q','Q','Q','K','K','K','K','A','A','A','A']
-        self.dealt_card = []
-        self.running_total = 0
-        
-    def randompick(self):
-        '''
-        Picks a random card from a deck
-        '''
-        random_card = random.choice(self.cards_in_deck)
-        self.cards_in_deck.remove(random_card)
+#        self.full_deck = fulldeck.full_deck
+       
+        self.d_card = []
+#        self.p_total_count = 0
 
 
-        if random_card == 'J':
-            self.dealt_card = 10
-            return self.running_total + 10
-        elif random_card == 'Q':
-            self.dealt_card = 10
-            return self.running_total + 10
-        elif random_card == 'K':
-            self.dealt_card = 10
-            return self.running_total + 10
+    def PlayersHand(self):
+        random_card = random.choice(DealHand.full_deck)
+        DealHand.full_deck.remove(random_card)
+        DealHand.p_card.append(random_card)
+        print(f"You've been dealt a {random_card}.")
+        if random_card in DealHand.full_deck[0:31]:
+            DealHand.p_total_count += int(random_card)
+        elif random_card == 'T' or random_card == 'J' or random_card == 'Q' or random_card == 'K':
+            DealHand.p_total_count += 10
         elif random_card == 'A':
-            self.dealt_card = int(input("Do you want your card to be worth 1 or 11? "))
-            if random_card == 1:
-                self.dealt_card = 1
-                return self.running_total + 1
-            elif random_card == 11:
-                self.dealt_card = 11
-                return self.running_total + 11
-        else:
-            self.dealt_card = random_card
-            return self.running_total + int(self.dealt_card)
+            pick_ace = int(input("Would you like your A to be worth 1 or 11? "))
+            if pick_ace == 1:
+                DealHand.p_total_count += 1
+            elif pick_ace == 11:
+                DealHand.p_total_count += 11   
 
-        print(self.dealt_card) 
-        print(f"Running total is: {self.running_total}")
-
-
-
-
-class Player(Deck):
-    """docstring for Player"""
-    def __init__(self, name, stack_size):
-        self.name = name
-        self.stack_size = stack_size
-
-    def official_stack_size(self):
-        print(f"You've bought in for ${self.stack_size}.")
-
-    def player_running_total(self):
-        print(f"You're running total is: {self.running_total}")
-
-class Dealer(Deck):
+    def DealersHand(self):
+        print("This is running")
+        random_card = random.choice(DealHand.full_deck)
+        DealHand.full_deck.remove(random_card)
+        DealHand.d_card.append(random_card)
+        if len(DealHand.d_card) < 2 or len(DealHand.d_card) > 2 :
+            print(f"The dealer's been dealt a {random_card}.")
+        if random_card is DealHand.full_deck[0:31]:
+            DealHand.d_total_count += random_card
+        elif random_card == 'T' or 'J' or 'Q' or 'K':
+            DealHand.d_total_count += 10
+        elif random_card == 'A':
+            DealHand.d_total_count += 1
+class Player(DealHand):
 
     def __init__(self):
-        self.running_total = 0
-
-    def dealer_running_total(self):
-        pass    
-
-class PlayersBet(Player):
-
-    def __init__(self,bet):
-        self.bet = bet
-
-    def bet_check(self):
-        if self.bet > self.stack_size:
-            print(f"Sorry, you don't have that much to bet. Pick an amount lower than {self.stack_size}")
-
-
-#class Dealer():
-    """docstring for Dealer"""
-  #  def __init__(self):
-     #   dealers_hand = []
-
-
-
-class DealtHand(Dealer):
-   
-    
-    def __init__(self):       
-        Deck.__init__(self)
-        self.players_hand = []
-        self.dealers_hand = []
-       # self.dealers_hand = []
-        self.players_total = sum(self.players_hand)
-       # dealers_hand = []
-      #  players_hand = []
-
-    def dealers_cards(self):
-
-
-        Dealer_Card1_SHOWN = Deck.randompick(self)
-        Dealer_Card2 = Deck.randompick(self)
-        return self.dealers_hand.extend([Dealer_Card1_SHOWN, Dealer_Card2])
-
-    def print_dealers_hand(self):
-        print(f"Dealer's hand: {self.running_total}")
-
+        pass
+#      self.p_hand = sum(self.p_card)
         
-
-    def players_cards(self, players_hand):
-
-        self.players_hand = players_hand
-
-  #      self.Player_Card1 = []
-  #      self.Player_Card2 = []
-
- #       players_hand = []
-
-        self.Player_Card1 = Deck.randompick(self)
-        self.players_hand.append(self.Player_Card1)  
-        self.Player_Card2 = Deck.randompick(self)
-        self.players_hand.append(self.Player_Card2)
-
-        print(f"players hand {self.players_hand}") 
-
-class NextMove(DealtHand,Deck):
-
-    def __init__(self):
-        Deck.__init__(self)
-        DealtHand.__init__(self)
-
-    def deal_next_card(self):
-
-  #      players_total = sum(self.players_hand)
-        print(self.players_total)
-        
+    def p_decision(self):       
         while True:
-            card_request = str(input(f"Your total is {self.players_total}. Would you like to Hit or Stay? "))
-            if card_request.capitalize() == 'Hit' and self.players_total <= 21:
-                Player_Card3 = Deck.randompick(self)
-                self.players_hand.append(Player_Card3)
-                
-            elif self.players_total > 21:
-                "YOU'VE BUSTED, YOU LOSE!"
+            hit_or_stay = str(input("Would you like to hit or stay? "))
+            if hit_or_stay.capitalize() == "Hit":
+                DealHand.PlayersHand(self)
+                print(f"You have a total of {DealHand.p_total_count}.")
+                Dealer.p_bust_check(self)
+                if DealHand.p_total_count > 21:
+                    break
+            elif hit_or_stay.capitalize() == "Stay":
+                print("Player stays. Good Luck!")
+                Dealer.d_decision(self)
                 break
             else:
                 break
 
-        return card_request
-        #   players_total = players_total + dealt_card
-        #elif card_request.capitalize() == 'Stay':  
-        
-     
-class CheckBust(DealtHand):
+#    def p_bust_check(self):
+#        if p_total_count > 21:
+#            print(f"You've busted, with a total of {p_total_count}. You lose.")
+
+    def __str__(self):
+        print(f"Thanks {self.name}, here's ${self.stack_size} in chips.")
+
+class Dealer():
 
     def __init__(self):
-        DealtHand.__init__(self)
-    #    DealtHand.dealers_cards(self)
-    #    DealtHand.players_cards(self)
-    
-    def player_bust_check(self):
-  #      players_total = sum(self.players_hand)
+        pass
 
-        if self.players_total > 21:
-            print("YOU'VE BUSTED, YOU LOSE!")
-            self.stack_size -= self.bet
+    def d_decision(self):
+        while True:
+            if DealHand.d_total_count < 17:
+                DealHand.DealersHand(self)
+                print(f"The Dealer has a total of {DealHand.d_total_count}.")
+            elif DealHand.d_total_count >= 17 and DealHand.d_total_count <= 21:
+                if DealHand.d_total_count > DealHand.p_total_count:
+                    print("You LOSE sucka! This is a Casino, you think you in Las Vegas Vacation or somethin?")
+                    print(f"The dealer has {DealHand.d_total_count}")
+                    break
+                elif DealHand.d_total_count == DealHand.p_total_count:
+                    print("IT'S A TIE. You and the Casino are besties.")
+                    print(f"The dealer has {DealHand.d_total_count}")
+                    break
+                elif DealHand.d_total_count > 21:
+                    print("The Casino Loses....for now sucka")
+                    print(f"The dealer has {DealHand.d_total_count}")
+                    break
+ #           elif DealHand.d_total_count > 21:
+ #               print( "Dealer BUSTS")
+ #               break
 
-    def dealer_bust_check(self):
-        dealers_total = sum(DealtHand.dealers_hand)
-
-        if dealers_total > 21:
-            print("THE DEALER HAS BUSTED, YOU WIN!")
-            self.stack_size += self.bet
-'''
-new class to test out git branching/commits
-'''
-
-class NewClassAppears():
-
-    my_list = []
-
-    def __init__(self,number, x, y):
-
-        self.x = x
-        self.y = y
-        self.number = number
-
-    def do_something(self):
-        print(f"Do something with {self.number}")
-    def multiply(self):
-        return self.x*self.y
+    def p_bust_check(self):
+        if DealHand.p_total_count > 21:
+            print(f"You've busted, with a total of {DealHand.p_total_count}. You lose.")
+        elif DealHand.p_total_count == 21:
+            print(f"HOLY SHIT SUCKA. 21! YOU WIN!")           
 
 if __name__ == '__main__':
-    
-        players_name = str(input("Hello there, what's your name? "))
-        players_stack = int(input("How much would you like to bring to the table? "))
+     
+    p_name = str(input("Hey there hot shot. What's your name? "))
+    p_stack_size = int(input(f"Greetings {p_name}. How much would you like to buy-in for? "))
+    print(f"Thanks {p_name} here's ${p_stack_size} in chips.")
+    bet_size = int(input(f"How much do you want to bet? "))
+    if bet_size >= (p_stack_size*.8):
+        print("\n\n\n\n\nB-B-b-baller!!\n\n\n\n\n")
+    else:
+        print("\n\n\n\n\nNo risk, no reward puss-ay. ...Anways, good luck\n\n\n\n\n")
 
-        the_deck = Deck()
-        the_deck.__init__()
-        player = Player(players_name,players_stack)
-        player.official_stack_size()
+while True:    
 
-        this_rounds_hand = DealtHand()
-        this_rounds_hand.dealers_cards()
-        this_rounds_hand.print_dealers_hand()
-        this_rounds_hand.players_cards(players_hand = [])
+    DealHand.p_total_count = 0
+    DealHand.d_total_count = 0
+    DealHand.d_card = []
 
-        next_move = NextMove()
-        next_move.deal_next_card()
+    print("***shuffle shuffle shuffle***   ***deal deal deal***\n\n\n")
+    player1 = Player()
 
-        bust = CheckBust()
-        bust.player_bust_check()
-        bust.dealer_bust_check()
-        
-  #      while True:
-   #         if sum(this_rounds_hand.players_hand) > 21:
-  #              CheckBust.player_bust_check()
-  #              break
-  #          elif sum(this_rounds_hand.dealers_hand) > 21: 
-  #              CheckBust.dealer_bust_check()
-  #              break
-  #          elif next_move.card_request == 'Hit':
-  #              next_move.deal_next_card()
-  #              continue
-  #          else:
-  #              break
+    p_deal_hand = DealHand()
+    p_deal_hand.PlayersHand()
+    p_deal_hand.PlayersHand()
+    p_deal_hand.DealersHand()
+    p_deal_hand.DealersHand()
+    print(f"The dealer is showing a {DealHand.d_card[(0)]}.\n")
+    print(f"You have a total of {DealHand.p_total_count}.\n")
+    player1.p_decision()
+    print(f"You have a total of {DealHand.p_total_count}.\n")
 
+  
+ #   dealer1 = Dealer()
+ #   dealer1.d_decision()
+
+    rematch = str(input("Would you like to play again? (Y/N) "))
+    if rematch.capitalize() == "Y" or rematch.capitalize() ==  "Yes":
+        int(input("You've chosen a rematch, how much do you want to bet? \n"))
+        print("\n\n\n\n\nNo more good luck...here are your cards. **THROWS CARDS IN DISGUST AT PATRON**\n\n\n\n\n")
+    elif rematch.capitalize() == "N" or rematch.capitalize() == "No":
+        break
 
 
